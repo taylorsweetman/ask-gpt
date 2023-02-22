7d97c6f34c79810c95925c4add1ddb551c300cda
Taylor Sweetman <taylor.sweetman@gmail.com>
2023-02-21 19:17:18 -0500

    Add argparse for temp arg

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ def setup():
     load_dotenv()
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    # load config
-    config = get_config()
-
     # parse args
     parser = argparse.ArgumentParser(
         description="Ask ChatGPT a question from your terminal."
@@ -25,7 +22,18 @@ def setup():
         nargs=1,
         help='your question prompt for ChatGPT, make sure you wrap the question in quotes (")',
     )
+    parser.add_argument(
+        "-t",
+        "--temperature",
+        type=float,
+        default=0,
+        nargs="?",
+        help="set temperature parameter",
+    )
     args = parser.parse_args()
+
+    # load config
+    config = get_config(args.temperature)
 
     return config, args
 
