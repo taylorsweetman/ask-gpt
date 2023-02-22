fda3621be5744b54aaaf654d6be117cdcfca1a1c
Taylor Sweetman <taylor.sweetman@gmail.com>
2023-02-21 19:16:54 -0500

    Pass temp to get_config

--- lib/config.py
+++ lib/config.py
@@ -3,7 +3,11 @@ import json
 from lib import Config
 
 
-def get_config() -> Config:
+def get_config(temperature: float = 0) -> Config:
     with open("config.json") as config_file:
         config = json.load(config_file)
-    return Config(**config)
+    return Config(
+        temperature=temperature,
+        max_tokens=config["max_tokens"],
+        completions_model=config["completions_model"],
+    )
